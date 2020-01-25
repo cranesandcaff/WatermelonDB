367ba025cbd51ca423a9a6ec48660dc7ade1fafe
Patrick Cauley <pcauley@nexient.com>
2020-01-25 11:20:22 -0500

    Updated make decorator type to fix type issue in decorators. If these are auto generated then it won't matter though.

--- src/utils/common/makeDecorator/index.d.ts
+++ src/utils/common/makeDecorator/index.d.ts
@@ -1,18 +1,24 @@
 declare module '@nozbe/watermelondb/utils/common/makeDecorator' {
-  import { ReplaceReturn } from '@nozbe/watermelondb/utils/common'
+  import { ReplaceReturn } from '@nozbe/watermelondb/utils/common';
 
-  export type Descriptor = Object
-  export type RawDecorator = (target: Object, key: string, descriptor: Descriptor) => Descriptor
-  export type RawDecoratorFactory<T extends any[]> = (...any: T) => RawDecorator
+  export type Descriptor = any;
+  export type RawDecorator = (
+    target: Object,
+    key: string,
+    descriptor: Descriptor,
+  ) => Descriptor;
+  export type RawDecoratorFactory<T extends any[]> = (
+    ...any: T
+  ) => RawDecorator;
 
   export type Decorator<
     Args extends any[],
     Factory extends RawDecoratorFactory<Args>
     // TODO: fix
-  > = ReplaceReturn<Args, Descriptor | RawDecorator, Factory>
+    > = any;
 
   export default function makeDecorator<
     Args extends any[],
     T extends RawDecoratorFactory<Args>
-  >(): Decorator<Args, T>
+  >(): Decorator<Args, T>;
 }
